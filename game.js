bc419d8151ba03d7fdf2fc910d60e8cf1da0a056
misthashira <122169549+misthashira@users.noreply.github.com>
2024-11-21 11:16:32 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,41 @@
+function playGame() {
+    const choices = [ "rock" ,"paper", "scissors"];
+    const playerChoice = document.getElementById("Player").value.toLowerCase();
+    
+    
+    if( !choices.includes(playerChoice))
+    {
+        document.getElementById("GameResult").textContent ="value entered is invalid Input Rock Paper or Scissors to Start game";
+        return;
+    }
+    const ComputerChoice = choices[Math.floor(Math.random () * choices.length)] ;
+
+    let Result ="";
+
+    if ( playerChoice === ComputerChoice){
+        Result = "its a tie!";
+    }
+
+
+    else if (  
+        (playerChoice === "rock" && ComputerChoice ==="scissors") ||
+        ( playerChoice==="paper" && ComputerChoice ==="rock") ||
+        (playerChoice ==="scissors" && ComputerChoice ==="paper")
+    )
+    {
+        Result ="Player Wins !"
+    }
+    else(
+        Result ="Computer Wins !"
+    )
+
+
+    document.getElementById("GameResult").innerHTML = `
+     YouChoose : <b> ${playerChoice} </b> <br>
+     computerChoose :<b>${ComputerChoice}</b><br>
+     <b> ${Result}</b>
+     ;`
+}
+
+
+
